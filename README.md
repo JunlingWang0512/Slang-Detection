2253b712753397c2e9d5f0117db6a315117a3b04
Feichi Lu <feiclu@student.ethz.ch>
2022-06-20 15:31:45 +0200

    augmentation_batch

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Slang-Detection
 Slang Detection, semantic course, ethz
+
+# Data Augmentation
+
+method: GPT2 + top_k/top_p/beam
+
+need to test which method and which parameter is better. 
+
+generation speed: about 4min on CPU, batchsize = 32.
+
+TODO:
+1. Find a method to test the generation quality. Do grid search on the parameters, find the dataset with best quality.
+2. Delete repeated generation results.
+3. If we can pretrain on GPT2 and then generate.
+4. Generate on other datasets.
---
+++ data_augmentation.py
@@ -0,0 +1,123 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import torch
+import pandas as pd
+from tqdm import tqdm
+import re
+
+# get device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+
+def GPT2_batch_generation(batch_list, max_gen_len, top_p, num_beams, method = 'top_k', top_k = 20, temperature = 0.5, num_return = 4):
+
+    inputs = tokenizer(batch_list, return_tensors="pt", padding=True)
+
+    if method == 'top_k':
+        output_sequences = model.generate(
+            input_ids=inputs['input_ids'].to(device),
+            attention_mask=inputs['attention_mask'].to(device),
+            do_sample=True, # disable sampling to test if batching affects output
+            max_length=len(inputs['input_ids'][0]) + max_gen_len, # let it generate longer
+            pad_token_id=tokenizer.eos_token_id,
+            top_k = top_k,
+            temperature=temperature,
+            num_return_sequences= num_return
+        )
+    if method == 'top_p':
+        output_sequences = model.generate(
+            input_ids=inputs['input_ids'].to(device),
+            attention_mask=inputs['attention_mask'].to(device),
+            do_sample=True, # disable sampling to test if batching affects output
+            max_length=len(inputs['input_ids'][0]) + max_gen_len, # let it generate longer
+            pad_token_id=tokenizer.eos_token_id,
+            top_p = top_p,
+            temperature=temperature,
+            num_return_sequences= num_return
+        )
+    if method == 'beam':
+        output_sequences = model.generate(
+            input_ids=inputs['input_ids'].to(device),
+            attention_mask=inputs['attention_mask'].to(device),
+            do_sample=True, # disable sampling to test if batching affects output
+            max_length=len(inputs['input_ids'][0]) + max_gen_len, # let it generate longer
+            pad_token_id=tokenizer.eos_token_id,
+            num_beams = num_beams,
+            temperature=temperature,
+            num_return_sequences= num_return
+        )
+
+    outputs = [tokenizer.decode(x, skip_special_tokens=True) for x in output_sequences]
+    return outputs
+
+
+def extract_sent(s, word, num):
+    s2 = ''
+    if(len(re.findall(str(num+1)+". "+str(word)+" :(.*?)\n", s)) >= 1):
+        s2 = re.findall(str(num+1)+". "+str(word)+" :(.*?)\n", s)[0]
+        if(len(re.findall("(.*?)"+str(word)+"(.*?)",s2)) >= 1):
+            s2 = s2.replace('\xa0', '')
+            s2 = s2.strip()
+    return s2
+
+
+# parameters
+batchsize = 32
+max_gen_len = 50
+
+method = 'top_k'
+top_k = 30
+top_p = 0.5
+num_beams = 5
+temperature = 0.5
+num_return = 6
+
+
+# GPT2 tokenizer and model
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+model = GPT2LMHeadModel.from_pretrained("gpt2", return_dict = True)
+# GPT2 parameter setting
+tokenizer.padding_side = "left" 
+tokenizer.pad_token = tokenizer.eos_token # to avoid an error
+
+# load trigger data file
+df_trigger = pd.read_csv('data/trigger_data.csv', index_col = 0)
+
+# generate result storage
+succeed = {'trigger': [], 'word': [], 'generate':[]}
+trigger_list = []
+word_list = []
+generated_list = []
+
+
+for i in tqdm(range(0, len(df_trigger), batchsize)):
+
+    # generation setting
+    batch = df_trigger.loc[i: i+batchsize-1, :]
+    batch_word = list(batch['word'])
+    batch_list = list(batch['trigger'])
+    batch_lens = list(batch['length'])
+
+    # generate sentences
+    outputs = GPT2_batch_generation(batch_list, max_gen_len, top_p, num_beams, method, top_k, temperature, num_return)
+
+
+    # extract sentences for each word
+    for j in range(len(batch)):
+        trigger = batch_list[j]
+        word = batch_word[j]
+        num = batch_lens[j]
+        for k in range(num_return*j, num_return*j+num_return):
+            s = extract_sent(str(outputs[k]), word, num)
+            if s != '':
+                trigger_list.append(trigger)
+                word_list.append(word)
+                generated_list.append(s)
+
+    # storage
+    if i%100 == 0 :
+        succeed['trigger'] = trigger_list
+        succeed['word'] = word_list
+        succeed['generate'] = generated_list
+        df_succeed = pd.DataFrame(succeed)
+        df_succeed.to_csv('data/succeed_batch.csv')
